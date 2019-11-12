e4d5ecbb2ffba820890a7f2d29fa498d28bb440a
Przemek <przemekpapla@gmail.com>
2019-11-12 12:18:04 +0100

    next lab

--- src/TCPtest.java
+++ src/TCPtest.java
@@ -5,9 +5,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
@@ -15,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class TCPtest {
@@ -24,11 +27,10 @@ public class TCPtest {
 		try {
 			LogManager.getLogManager().readConfiguration(stream);
 		} catch (SecurityException | IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
 		exec.scheduleAtFixedRate(() -> {
@@ -37,66 +39,139 @@ public class TCPtest {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}, 1, 2, TimeUnit.SECONDS);
-		
+		}, 1, 5, TimeUnit.SECONDS);
+
 		try {
 			TCPServer.main(null);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	private static final Logger logger = Logger.getLogger(TCPtest.class.getName());
 }
 
 class TCPServer {
-	static BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(1);
-	
+//	static BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(1);
+	private static List<ClientThread> clients = new ArrayList<>();
+
 	public static void main(String argv[]) throws Exception {
-		String clientSentence;
-		String capitalizedSentence;
-		ServerSocket welcomeSocket = new ServerSocket(6722);
-		queue.add(1);
+		new TCPServer();
+	}
+
+	public TCPServer() {
+//		String clientSentence;
+//		String capitalizedSentence;
+		ServerSocket welcomeSocket;
+		try {
+			welcomeSocket = new ServerSocket(6722);
+		} catch (IOException e) {
+			return;
+		}
+
+//		ExecutorService service = Executors.newSingleThreadExecutor();
 
 		while (!welcomeSocket.isClosed()) {
-			Socket connectionSocket = welcomeSocket.accept();
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-			clientSentence = inFromClient.readLine();
-			
-			logger.info("RECEIVED: " + clientSentence);
-			logger.info(Arrays.toString(clientSentence.chars().toArray()));
-			capitalizedSentence = clientSentence.toUpperCase() + '\n';
-			outToClient.writeBytes(capitalizedSentence);
+			Socket socket = null;
+			try {
+				socket = welcomeSocket.accept();
+			} catch (IOException e) {
+				logger.info(e.getMessage());
+				break;
+			}
+			ClientThread clientThread = this.new ClientThread(socket);
+			clients.add(clientThread);
+			clientThread.start();
+
+//			clients.add(welcomeSocket.accept());
+//			Socket connectionSocket = welcomeSocket.accept();
+//			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+//			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+//			clientSentence = inFromClient.readLine();
+//			
+//			logger.info("RECEIVED: " + clientSentence);
+//			logger.info(Arrays.toString(clientSentence.chars().toArray()));
+//			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+//			outToClient.writeBytes(capitalizedSentence);
 		}
-		
-		welcomeSocket.close();
+
+		try {
+			welcomeSocket.close();
+		} catch (IOException e) {
+			logger.info(e.getMessage());
+		}
 	}
-	
+
 	class ClientThread extends Thread {
+		private String clientSentence;
+		private String capitalizedSentence;
 		private Socket socket;
-		
+
 		ClientThread(Socket socket) {
 			this.socket = socket;
 		}
+
+		@Override
+		public void start() {
+			super.start();
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+					clientSentence = inFromClient.readLine();
+					inFromClient.wait(0);
+					if (clientSentence != null) {
+						logger.info("RECEIVED: " + clientSentence);
+						logger.info(Arrays.toString(clientSentence.chars().toArray()));
+						capitalizedSentence = clientSentence.toUpperCase() + '\n';
+						outToClient.writeBytes(capitalizedSentence);
+					}
+				} catch (IOException e) {
+					logger.info(e.getMessage());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
-	
+
 	private static final Logger logger = Logger.getLogger(TCPServer.class.getName());
 }
 
 class TCPClient {
 	public static void main(String argv[]) throws Exception {
-		String sentence;
-		String modifiedSentence;
-		Socket clientSocket = new Socket("localhost", 6722);
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		sentence = "Hello" + (char) (new Random().nextInt(24) + 68);
-		outToServer.writeBytes(sentence + '\n');
-		modifiedSentence = inFromServer.readLine();
-		logger.info("FROM SERVER: " + modifiedSentence);
-		clientSocket.close();
+		new Thread(() -> {
+			// String sentence;
+			// String modifiedSentence;
+			try {
+				Socket clientSocket = new Socket("localhost", 6722);
+				DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+				BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+				IntStream.range(0, 5).forEach((i) -> {
+					try {
+						String sentence = "Hello" + i + (char) (new Random().nextInt(24) + 68);
+						outToServer.writeBytes(sentence + '\n');
+						outToServer.notify();
+						String modifiedSentence = inFromServer.readLine();
+						logger.info("FROM SERVER: " + modifiedSentence);
+						Thread.sleep(100);
+					} catch (InterruptedException | IOException e) {
+
+					}
+				});
+				clientSocket.close();
+			} catch (Exception e) {
+				logger.info(e.getMessage());
+			}
+		}).start();
+		;
 		return;
 	}
-	
+
 	private static final Logger logger = Logger.getLogger(TCPClient.class.getName());
 }
